9c949b04341d2fefd0945bdf5f1133e43af76d0a
Antonio Madera <madera.amg@gmail.com>
2017-11-10 23:31:27 +0100

    Adds external file js bootstrap v4 library.

--- add-bootstrap-v4.php
+++ add-bootstrap-v4.php
@@ -14,7 +14,11 @@ class AddBootstrap4 {
     
     private $hookAddNewFile = 'wp_enqueue_scripts';
     private $cssFolder = 'css/';
+    private $jsFolder = 'js/';
     private $cssFileName = 'bootstrap.min.css';
+    private $jsJqueryFileName = 'jquery-3.1.1.slim.min.js';
+    private $jsTetherFileName = 'tether.min.js';
+    private $jsBootstrapFileName = 'bootstrap.min.js';
 
     public function __construct() {
         $this->run();
@@ -22,6 +26,7 @@ class AddBootstrap4 {
 
     private function run() {
         $this->addCssFile();
+        $this->addJsFiles();
     }
 
     private function addCssFile() {
@@ -30,7 +35,22 @@ class AddBootstrap4 {
 
     public function getEnqueueCssFileUrl() {
         $cssUrl = plugins_url($this->cssFolder . $this->cssFileName, __FILE__);
+        
         wp_enqueue_style('bootstrap-css', $cssUrl);
+    }
+
+    private function addJsFiles() {
+        add_action($this->hookAddNewFile, [$this, 'getEnqueueJsFilesUrl']);
+    }
+
+    public function getEnqueueJsFilesUrl() {
+        $jsJqueryUrl = plugins_url($this->jsFolder . $this->jsJqueryFileName, __FILE__);
+        $jsTetherUrl = plugins_url($this->jsFolder . $this->jsTetherFileName, __FILE__);
+        $jsBootstrapUrl = plugins_url($this->jsFolder . $this->jsBootstrapFileName, __FILE__);
+
+        wp_enqueue_script('jquery-js', $jsJqueryUrl, [], '', true);
+        wp_enqueue_script('tether-js', $jsTetherUrl, [], '', true);
+        wp_enqueue_script('bootstrap-js', $jsBootstrapUrl, [], '', true);
     }
 }
 
