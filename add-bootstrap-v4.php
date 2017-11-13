ede1b2911d83d99267b8eae5cfcc3bda33b93107
Antonio Madera <madera.amg@gmail.com>
2017-11-13 23:23:12 +0100

    Menu added in Admin area.
    Load page in admin from template.
    
    Set way to load css file from admin.
    
    Add style as label HTML in head. If option is set in WP Admin, add style as external link to file.
    
    Dynamic text in WP Admin template.

--- add-bootstrap-v4.php
+++ add-bootstrap-v4.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Add Bootstrap v4
  * Plugin URI: http://antoniomadera.com
- * Version: 0.0.1
+ * Version: 1.0
  * Description: This plugin adds bootstrap version 4 to your Wordpress installation
  * Author: Antonio Madera
  * Author URI: http://antoniomadera.com
@@ -12,25 +12,59 @@
 
 class AddBootstrap4 {
     
+    private static $cssOptionValue = 'external_css_file';
+    private static $pluginOptionsGroupValue = 'add-bootstrap-css-library';
+    private static $submenuName = 'CSS Bootstrap Library';
+    
     private $hookAddNewFile = 'wp_enqueue_scripts';
+    private $hookHeadHtml = 'wp_head';
+    private $hookAddAdminMenuToDashboard = 'admin_menu';
+    private $hookInit = 'init';
     private $cssFolder = 'css/';
     private $jsFolder = 'js/';
+    private $templatesFolder = 'templates/';
     private $cssFileName = 'bootstrap.min.css';
     private $jsJqueryFileName = 'jquery-3.1.1.slim.min.js';
     private $jsTetherFileName = 'tether.min.js';
     private $jsBootstrapFileName = 'bootstrap.min.js';
+    private $menuPageFile = 'options-general.php';
+    private $menuCapability = 'manage_options';
+    private $menuSlug = 'css-bootstrap-library';
+    private $adminTemplateFile = 'admin-index.php';
 
     public function __construct() {
         $this->run();
     }
 
     private function run() {
-        $this->addCssFile();
-        $this->addJsFiles();
+        $this->registerValues();
+        
+        if (is_admin()) {
+            $this->addAdminMenu();
+        } else {
+            $this->addCssFile();
+            $this->addJsFiles();
+        }
     }
 
     private function addCssFile() {
-        add_action($this->hookAddNewFile, [$this, 'getEnqueueCssFileUrl']);
+        if (get_option(self::$cssOptionValue)) {
+            add_action($this->hookAddNewFile, [$this, 'getEnqueueCssFileUrl']);
+        } else {
+            add_action($this->hookHeadHtml, [$this, 'printCssContent']);
+        }
+    }
+
+    private function addJsFiles() {
+        add_action($this->hookAddNewFile, [$this, 'getEnqueueJsFilesUrl']);
+    }
+
+    private function addAdminMenu() {
+        add_action($this->hookAddAdminMenuToDashboard, [$this, 'addAdminMenuToDashboard']);
+    }
+
+    private function registerValues() {
+        add_action($this->hookInit, [$this, 'registerCSSValues']);
     }
 
     public function getEnqueueCssFileUrl() {
@@ -39,8 +73,10 @@ class AddBootstrap4 {
         wp_enqueue_style('bootstrap-css', $cssUrl);
     }
 
-    private function addJsFiles() {
-        add_action($this->hookAddNewFile, [$this, 'getEnqueueJsFilesUrl']);
+    public function printCssContent() {
+        $filePathname = plugin_dir_path(__FILE__) . $this->cssFolder . $this->cssFileName;
+        
+        echo '<style type="text/css">' . file_get_contents($filePathname) . '</style>';
     }
 
     public function getEnqueueJsFilesUrl() {
@@ -51,6 +87,36 @@ class AddBootstrap4 {
         wp_enqueue_script('jquery-js', $jsJqueryUrl, [], '', true);
         wp_enqueue_script('tether-js', $jsTetherUrl, [], '', true);
         wp_enqueue_script('bootstrap-js', $jsBootstrapUrl, [], '', true);
+    }
+
+    public function addAdminMenuToDashboard() {
+        add_submenu_page($this->menuPageFile, self::$submenuName, self::$submenuName, $this->menuCapability, $this->menuSlug, [$this, 'pageInMenu']);
+    }
+
+    public function pageInMenu() {
+        $filePathname = plugin_dir_path(__FILE__) . $this->templatesFolder . $this->adminTemplateFile;
+        
+        load_template($filePathname);
+    }
+
+    public function registerCSSValues() {
+        register_setting(self::$pluginOptionsGroupValue, self::$cssOptionValue);
+    }
+
+    public static function getExternalOptionName() {
+        return self::$cssOptionValue;
+    }
+
+    public static function getOptionsGroupValue() {
+        return self::$pluginOptionsGroupValue;
+    }
+
+    public static function getH2Title() {
+        return self::$submenuName . ' Settings';
+    }
+
+    public static function getPluginName() {
+        return 'Add ' . self::$submenuName . ' plugin';
     }
 }
 
